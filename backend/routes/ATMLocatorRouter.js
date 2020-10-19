3322868bde829371dba7c8e2a1e232d86e19d4ba
Sashankh Chengavalli Kumar <cksashankh@gmail.com>
2020-10-19 09:35:36 +0800

    Mock card, ATM and merchant APIs

--- backend/routes/ATMLocatorRouter.js
+++ backend/routes/ATMLocatorRouter.js
@@ -2,8 +2,31 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/search', function (req, res) {
-    res.end('ATM search end point reached');
+router.post('/search', function (req, res) {
+    let { latitude, longitude, radius, maxResults} = req.body;
+    res.json({
+        "results": [
+            {
+                name: "ATM 1",
+                streetAddress: "1 Sample Road, 123456",
+                latitude: "123.123",
+                longitude: "12.12"
+            },
+            {
+                name: "ATM 2",
+                streetAddress: "1 Sample Road, 123456",
+                latitude: "123.123",
+                longitude: "12.12"
+            },
+            {
+                name: "ATM 3",
+                streetAddress: "1 Sample Road, 123456",
+                latitude: "123.123",
+                longitude: "12.12"
+            }
+        ]
+    });
+    res.end();
 });
 
 module.exports = router;
--- backend/routes/merchantLocatorRouter.js
+++ backend/routes/merchantLocatorRouter.js
@@ -2,8 +2,34 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/search', function (req, res) {
-    res.end('Merchant search end point reached');
+router.post('/search', function (req, res) {
+    let { latitude, longitude, radius, maxResults } = req.body;
+    res.json({
+        "results": [
+            {
+                name: "Store 1",
+                category: "Supermarket",
+                streetAddress: "1 Sample Road, 123456",
+                latitude: "123.123",
+                longitude: "12.12"
+            },
+            {
+                name: "Store 2",
+                category: "Gas Station",
+                streetAddress: "1 Sample Road, 123456",
+                latitude: "123.123",
+                longitude: "12.12"
+            },
+            {
+                name: "Store 3",
+                category: "Restaurant",
+                streetAddress: "1 Sample Road, 123456",
+                latitude: "123.123",
+                longitude: "12.12"
+            }
+        ]
+    });
+    res.end();
 });
 
 module.exports = router;
