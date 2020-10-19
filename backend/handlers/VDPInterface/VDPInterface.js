29e82f18f94d60f9b61342c69f9c3f3cd98bb365
Sashankh Chengavalli Kumar <cksashankh@gmail.com>
2020-10-19 23:57:52 +0800

    Implement request to VDP platform
     * requests library used
     * credentials provided
     * response returned
     * parameterization with request values not done

--- backend/handlers/VDPInterface/VDPInterface.js
+++ backend/handlers/VDPInterface/VDPInterface.js
@@ -1,34 +1,63 @@
+const https = require('follow-redirects').https;
+const fs = require('fs');
+
 class VDPInterface {
     constructor (url, credentialsPath) {
         this.url = url;
-        this.credentialsPath = credentialsPath;
+        this.credentials = require(credentialsPath);
+        let { userName, password } = this.credentials;
+        this.authValue = "Basic " + new Buffer(userName + ":" + password).toString("base64");
+        console.log(this.authValue);
     }
-
+    
     startCard(PAN, secret) {
         // Not required
     }
-
+    
     stopCard(PAN, secret) {
         // Not required
     }
-
+    
     getTravelPredictions() {
-
+        
     }
-
+    
     searchForATM() {
-
+        
     }
-
+    
+    // TODO: Remove hard coding
     searchForMerchant() {
+        console.log("Searching for merchant");
+        
+        var request = require('request');
+        var options = {
+            'method': 'POST',
+            'url': 'https://sandbox.api.visa.com/merchantlocator/v1/locator',
+            'headers': {
+                'Authorization': 'Basic TEFYQTdNMVlGRUhSNzhIQjhDWkEyMU1xQnl0ZjhwaGJzX3duYTJZdkpfZ3FUVjd5Yzp5QWtqZHFFOUtOSGdPM2U0eDNkMjA1bGU0bXo=',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({"searchOptions":{"matchScore":"true","maxRecords":"5","matchIndicators":"true"},"header":{"startIndex":"0","requestMessageId":"Request_001","messageDateTime":"2020-10-19T09:19:39.903"},"searchAttrList":{"distanceUnit":"M","distance":"2","merchantCountryCode":"840","latitude":"37.363922","longitude":"-121.929163","merchantName":"Starbucks"},"responseAttrList":["GNLOCATOR"]}),
+            'cert': fs.readFileSync('./handlers/VDPInterface/cert.pem'),
+            'key': fs.readFileSync('./handlers/VDPInterface/key.pem')
+        };
 
+        return new Promise ((resolve, reject) => {
+            request(options, function (error, response) {
+                if (error) reject(error);
+                resolve(response.body);    
+            })
+        });
     }
-
+    
     addTravelItinerary() {
-
+        
     }
-
+    
     removeTravelItinerary() {
         // Not required
     }
 }
+
+module.exports = new VDPInterface("https://sandbox.api.visa.com", "./credentials.json")
--- backend/routes/merchantLocatorRouter.js
+++ backend/routes/merchantLocatorRouter.js
@@ -1,35 +1,43 @@
 const express = require('express');
+const vdpInterfaceInstance = require('../handlers/VDPInterface/VDPInterface');
 
 const router = express.Router();
 
 router.post('/search', function (req, res) {
     let { latitude, longitude, radius, maxResults } = req.body;
-    res.json({
-        "results": [
-            {
-                name: "Store 1",
-                category: "Supermarket",
-                streetAddress: "1 Sample Road, 123456",
-                latitude: "123.123",
-                longitude: "12.12"
-            },
-            {
-                name: "Store 2",
-                category: "Gas Station",
-                streetAddress: "1 Sample Road, 123456",
-                latitude: "123.123",
-                longitude: "12.12"
-            },
-            {
-                name: "Store 3",
-                category: "Restaurant",
-                streetAddress: "1 Sample Road, 123456",
-                latitude: "123.123",
-                longitude: "12.12"
-            }
-        ]
+    console.log("Calling searchForMerchant()");
+    vdpInterfaceInstance.searchForMerchant().then((result) => {
+        res.json(JSON.parse(result));
+        res.end();
+    }).catch(error => {
+        console.log("Error is: " + error);
     });
-    res.end();
+    // res.json({
+    //     "results": [
+    //         {
+    //             name: "Store 1",
+    //             category: "Supermarket",
+    //             streetAddress: "1 Sample Road, 123456", 
+    //             latitude: "123.123",
+    //             longitude: "12.12"
+    //         },
+    //         {
+    //             name: "Store 2",
+    //             category: "Gas Station",
+    //             streetAddress: "1 Sample Road, 123456",
+    //             latitude: "123.123",
+    //             longitude: "12.12"
+    //         },
+    //         {
+    //             name: "Store 3",
+    //             category: "Restaurant",
+    //             streetAddress: "1 Sample Road, 123456",
+    //             latitude: "123.123",
+    //             longitude: "12.12"
+    //         }
+    //     ]
+    // });
+    
 });
 
 module.exports = router;
